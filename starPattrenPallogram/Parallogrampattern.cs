f54e60d3a9020820a272fe1ae19d8fad8006f5c5
Hemantdhurve <hemantdhurve007@gmail.com>
2022-09-25 22:04:17 +0530

    [Hemant] Added Star Pattern Parallelogram workshop 2 Problem

---
+++ starPattrenPallogram/Parallogrampattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace starPattrenPallogram
+{
+    public class Parallogrampattern
+    {
+        public void parallogram()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = i; j <= 4; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k =1 ; k <= 5; k++)
+                {
+                    Console.Write("*");
+                }
+               
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }       
+    }
+        
+}
---
+++ starPattrenPallogram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace starPattrenPallogram
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Star Pattern Parallelogram");
+
+            Parallogrampattern star = new Parallogrampattern();
+            star.parallogram();
+            
+        }
+    }
+}
